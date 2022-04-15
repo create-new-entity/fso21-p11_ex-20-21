507a3b665e3b768fa58dc649334a7056cdce1ee9
imran-pavel <imran.pavel@gmail.com>
2022-04-15 18:19:41 +0300

    Update test_helper.js

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -67,7 +67,7 @@ const login = async (api, user) => {
       password: user.password
     });
   console.log('user', user);
-  console.log('result: ', res);
+  console.log('result: ', res.body);
   return res.body  // { username, id, token }
 };
 
