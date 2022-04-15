2608867c406c1b8ab083ac8acb7fe047369a7e80
imran-pavel <imran.pavel@gmail.com>
2022-04-15 17:18:41 +0300

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
-console.log(`TEST_DB_URL: ${process.env.TEST_DB_URL}`)
+console.log(`TEST_DB_URL: ${process.env.TEST_DB_URL}`);
+console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
 
 const PORT = process.env.PORT;
 const DB_URL = process.env.NODE_ENV === 'test'
