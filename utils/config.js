142e2a25ab9c50c951479fc583d1b47fcf99eebe
imran-pavel <imran.pavel@gmail.com>
2022-04-16 12:05:19 +0300

    Fixed + Uncommented
    Environment variables had to be prefixed with 'HD_' like this: 'HD_HEROKU_API_KEY' for 'akhileshns/heroku-deploy@v3.12.12' action in the pipeline.

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-console.log(process.env.DB_URL ? 'DB_URL ok' : 'DB_URL not ok');
-
 const PORT = process.env.PORT;
 const DB_URL = process.env.NODE_ENV === 'test'
   ? process.env.TEST_DB_URL
